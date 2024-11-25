fc37430dac5ac6455f5f060b6e823525a6c1a067
Kanwarjeet Khalsa <kenny.khalsa@gmail.com>
2024-11-25 12:09:33 +0000

    converting typescript to js during compilation time

--- plugins/ngrx-signals-store/generator.ts
+++ plugins/ngrx-signals-store/generator.ts
@@ -1,34 +1,17 @@
-import {
+const {
   formatFiles,
   generateFiles,
   getWorkspaceLayout,
   names,
   offsetFromRoot,
   Tree,
-} from '@nx/devkit';
-import * as path from 'path';
-import { SignalsStoreGeneratorSchema } from './schema';
+} = require('@nx/devkit');
+const path = require('path');
 
-interface StateProperty {
-  name: string;
-  type: string;
-}
+// Remove the type import and definition since they won't be available at runtime
+// The schema types will be handled by TypeScript during development only
 
-interface NormalizedSchema extends SignalsStoreGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedPath: {
-    name: string;
-    path: string;
-  };
-  angularService: string;
-  angularServiceMethod: string;
-  statePropertiesParsed: StateProperty[];
-  fileName: string;
-}
-
-function parseStateProperties(stateProperties = ''): StateProperty[] {
+function parseStateProperties(stateProperties = '') {
   if (!stateProperties) return [];
   
   return stateProperties.split(',').map(prop => {
@@ -37,7 +20,7 @@ function parseStateProperties(stateProperties = ''): StateProperty[] {
   });
 }
 
-function normalizeOptions(tree: Tree, options: SignalsStoreGeneratorSchema): NormalizedSchema {
+function normalizeOptions(tree: any, options: { name: any; path: string; angularService: any; angularServiceMethod: any; stateProperties: string | undefined; }) {
   const name = names(options.name).fileName;
   const projectDirectory = options.path ?? '';
   const projectName = name;
@@ -64,7 +47,7 @@ function normalizeOptions(tree: Tree, options: SignalsStoreGeneratorSchema): Nor
   };
 }
 
-function addFiles(tree: Tree, options: NormalizedSchema) {
+function addFiles(tree: { exists: (arg0: any) => any; rename: (arg0: any, arg1: any) => void; }, options: { name: any; projectRoot: any; parsedPath: { path: any; }; fileName: any; }) {
   const templateOptions = {
     ...options,
     ...names(options.name),
@@ -92,8 +75,13 @@ function addFiles(tree: Tree, options: NormalizedSchema) {
   }
 }
 
-export default async function (tree: Tree, options: SignalsStoreGeneratorSchema) {
+async function generator(tree: any, options: any) {
   const normalizedOptions = normalizeOptions(tree, options);
   addFiles(tree, normalizedOptions);
   await formatFiles(tree);
 }
+
+// Export the generator as the default export
+module.exports = generator;
+module.exports.normalizeOptions = normalizeOptions;
+module.exports.addFiles = addFiles;
--- plugins/ngrx-signals-store/schema.d.ts
+++ plugins/ngrx-signals-store/schema.d.ts
@@ -1,10 +1,32 @@
+import { Tree } from '@nx/devkit';
+
 export interface SignalsStoreGeneratorSchema {
   name: string;
-  project: string;
-  path: string;
-  angularService: string;
-  angularServiceMethod: string;
-  prefix?: string;
-  skipTests?: boolean;
+  path?: string;
+  angularService?: string;
+  angularServiceMethod?: string;
   stateProperties?: string;
 }
+
+export interface StateProperty {
+  name: string;
+  type: string;
+}
+
+export interface NormalizedSchema extends SignalsStoreGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  parsedPath: {
+    name: string;
+    path: string;
+  };
+  angularService: string;
+  angularServiceMethod: string;
+  statePropertiesParsed: StateProperty[];
+  fileName: string;
+}
+
+export function normalizeOptions(tree: Tree, options: SignalsStoreGeneratorSchema): NormalizedSchema;
+export function addFiles(tree: Tree, options: NormalizedSchema): void;
+export default function(tree: Tree, options: SignalsStoreGeneratorSchema): Promise<void>;
